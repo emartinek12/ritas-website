8301f5791d9e4a8cefc6b89989ab91207c540544
Anna Waldorf <anna@waldorf.de>
2022-03-07 09:02:52 +0100

    still working on the mobile burger menu

--- scripts/script.js
+++ scripts/script.js
@@ -18,11 +18,49 @@ window.onscroll = () => {
 };
 
 
-console.log("works maybe")
 const hamburger = document.getElementById('hamburger');
 const navUL = document.getElementById('nav-ul');
+const navitem = document.getElementsByClassName('nav-item');
 
+console.log("works maybe")
 hamburger.addEventListener('click', () => {
   navUL.classList.toggle('show');
-  console.log("works")
+  console.log("collapse")
 });
+
+const menu = document.getElementById('nav-ul');
+const link = document.getElementsByClassName('nav-item');
+
+link.on('click', () => {
+  menu.classList.remove('show');
+});
+
+
+console.log("remove menu loading")
+console.log("remove menu worked")
+
+
+// $('.hamburger').on('click', function () {
+//   $('.menu').addClass('open');
+// });
+
+// $('.menu a').on("click", function () {
+//   $('.menu').removeClass('open');
+// });
+
+
+// const dropdown = document.getElementById('nav-ul');
+// const item = jQuery('.nav-ul li')
+
+// item.addEventListener('click', () => {
+//   dropdown.classList.toggle('hide');
+//   console.log("mobile navbar hidden")
+// });
+
+// $(function () {
+//   $('.nav a').on('click', function () {
+//     if ($('.navbar-toggle').css('display') != 'none') {
+//       $(".navbar-toggle").trigger("click");
+//     }
+//   });
+// });
