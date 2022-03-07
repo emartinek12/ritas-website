a2e63cde47225cc013200c1a238018120dfc6115
Anna Waldorf <anna@waldorf.de>
2022-03-07 14:01:40 +0100

    fixed mobile navbar finally

--- scripts/script.js
+++ scripts/script.js
@@ -17,50 +17,20 @@ window.onscroll = () => {
   });
 };
 
+// making navbar collapse with burger menu when mobile
 
 const hamburger = document.getElementById('hamburger');
 const navUL = document.getElementById('nav-ul');
 const navitem = document.getElementsByClassName('nav-item');
 
-console.log("works maybe")
 hamburger.addEventListener('click', () => {
   navUL.classList.toggle('show');
-  console.log("collapse")
 });
 
-const menu = document.getElementById('nav-ul');
-const link = document.getElementsByClassName('nav-item');
+// remove navbar after having clicked on one sections-link
 
-link.on('click', () => {
-  menu.classList.remove('show');
+$(".nav-ul a").click(function () {
+  console.log("clicked a")
+
+  $(".nav-ul").removeClass('show');
 });
-
-
-console.log("remove menu loading")
-console.log("remove menu worked")
-
-
-// $('.hamburger').on('click', function () {
-//   $('.menu').addClass('open');
-// });
-
-// $('.menu a').on("click", function () {
-//   $('.menu').removeClass('open');
-// });
-
-
-// const dropdown = document.getElementById('nav-ul');
-// const item = jQuery('.nav-ul li')
-
-// item.addEventListener('click', () => {
-//   dropdown.classList.toggle('hide');
-//   console.log("mobile navbar hidden")
-// });
-
-// $(function () {
-//   $('.nav a').on('click', function () {
-//     if ($('.navbar-toggle').css('display') != 'none') {
-//       $(".navbar-toggle").trigger("click");
-//     }
-//   });
-// });
